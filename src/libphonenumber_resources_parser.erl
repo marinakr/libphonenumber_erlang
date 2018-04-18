617bca97acc44b4ad663c591d0bbfe5455862f3d
Maryna Shabalina <m.shabalina@tiwinnovations.com>
2018-04-18 19:04:33 +0300

    init carrier parser

---
+++ src/libphonenumber_resources_parser.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author user
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 18. Apr 2018 4:07 PM
+%%%-------------------------------------------------------------------
+-module(libphonenumber_resources_parser).
+-author("user").
+
+%% API
+-export([]).
