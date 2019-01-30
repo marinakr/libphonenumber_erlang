8d7cac93fb9e8187a55f8a4ac84f50e80e9d0fde
Maryna <marinakrasnovatr81@gmail.com>
2019-01-31 00:21:30 +0200

    chore: more tests

--- test/libphonenumbers_SUITE.erl
+++ test/libphonenumbers_SUITE.erl
@@ -4,12 +4,13 @@
 -include_lib("common_test/include/ct.hrl").
 
 -export([all/0]).
--export([init_per_suite/1, mod_exists/1, is_mobile_valid_phone_test/1, end_per_suite/1]).
+-export([init_per_suite/1, mod_exists/1, is_mobile_valid_phone_test/1, mobile_phone_number_info_test/1, end_per_suite/1]).
 
 all() ->
   [
   mod_exists,
-  is_mobile_valid_phone_test
+  is_mobile_valid_phone_test,
+  mobile_phone_number_info_test
   ].
 
 init_per_suite(Config) ->
@@ -31,5 +32,12 @@ is_mobile_valid_phone_test(Config) ->
   PhoneExamples = proplists:get_value(valid_phone_examples, Config),
   Valid = lists:all(fun libphonenumbers:is_mobile_valid_phone/1, PhoneExamples),
   ?assert(Valid).
+
+mobile_phone_number_info_test(_Config) ->
+  #{country_metadata :=
+     #{code := <<"380">>,id := <<"UA">>,name := <<"Ukraine">>},
+     phone := <<"+380967112244">>,valid := true} = libphonenumbers:mobile_phone_number_info(<<"+380967112244">>),
+  #{country_metadata := #{code := <<"1">>, id := <<"US">>, name := <<"United States">>},
+    errors := [],phone := <<"+14088881406">>, valid := true} = libphonenumbers:mobile_phone_number_info(<<"+14088881406">>).
 
 end_per_suite(_Config) -> [].
